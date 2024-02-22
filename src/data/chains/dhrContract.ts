73e0e7b01eaaa0d7212a5c6ffd9739b2ebed57d4
Weagain <chinaren.back@gmail.com>
2024-02-22 11:53:11 +0800

    update abi&address

--- src/data/chains/dhrContract.ts
+++ src/data/chains/dhrContract.ts
@@ -1,4 +1,5 @@
-const crossTokenABI = [
+const generalABI = [{"inputs":[{"internalType":"uint256","name":"_betCost","type":"uint256"},{"internalType":"address","name":"_devTreater","type":"address"},{"internalType":"uint256","name":"_roundFee","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"round","type":"uint256"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"address","name":"inviter","type":"address"},{"indexed":false,"internalType":"uint256","name":"seq","type":"uint256"}],"name":"RoundJoin","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"round","type":"uint256"},{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"RoundPrizeEx","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"uint256","name":"round","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"count","type":"uint256"},{"indexed":false,"internalType":"address[]","name":"users","type":"address[]"}],"name":"RoundWinner","type":"event"},{"inputs":[],"name":"INVITE_SHARE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"ROUND_COUNT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"ROUND_FEE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"betCost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"canRebid","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"currentRound","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"devTreater","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"manager","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"roundFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"roundPartInviter","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"roundPartVerifyer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"roundParticipants","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"roundPrize","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"roundSettling","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"roundWinCode","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"roundWinner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"stateMutability":"payable","type":"receive","payable":true},{"inputs":[{"internalType":"address","name":"_inviter","type":"address"}],"name":"enjoy","outputs":[],"stateMutability":"payable","type":"function","payable":true},{"inputs":[],"name":"currentRoundInfo","outputs":[{"components":[{"internalType":"uint256","name":"index","type":"uint256"},{"internalType":"address[]","name":"users","type":"address[]"},{"internalType":"uint256","name":"prize","type":"uint256"},{"internalType":"address[]","name":"winners","type":"address[]"},{"internalType":"uint256","name":"wincode","type":"uint256"}],"internalType":"struct GeneralDestinyHashRing.RoundInfo","name":"data","type":"tuple"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"_hisRound","type":"uint256"}],"name":"historyRoundInfo","outputs":[{"components":[{"internalType":"uint256","name":"index","type":"uint256"},{"internalType":"address[]","name":"users","type":"address[]"},{"internalType":"uint256","name":"prize","type":"uint256"},{"internalType":"address[]","name":"winners","type":"address[]"},{"internalType":"uint256","name":"wincode","type":"uint256"}],"internalType":"struct GeneralDestinyHashRing.RoundInfo","name":"data","type":"tuple"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"_newManager","type":"address"},{"internalType":"bytes32","name":"hash","type":"bytes32"},{"internalType":"bytes","name":"signature","type":"bytes"}],"name":"transferManager","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newBidCost","type":"uint256"},{"internalType":"uint256","name":"_newFee","type":"uint256"}],"name":"recBF","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newTreater","type":"address"}],"name":"transferDevTreater","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"wallet","type":"address"},{"internalType":"uint256","name":"comw","type":"uint256"}],"name":"testCode","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"pure","type":"function","constant":true}]
+const blastABI = [
   {
     inputs: [
       {
@@ -537,36 +538,36 @@ export interface NetModel {
   contractAbi: any[]
 }
 export const supportNetworks: NetModel[] = [
-  // {
-  //   chainId: 7777777,
-  //   chainName: 'ZORA Network',
-  //   chainSymbol: 'Zora',
-  //   coin: 'ETH',
-  //   textColor: '#FFFFFF',
-  //   color: '#000000',
-  //   contractAddr: '0x111cdBF288C9613019c9c324D9E9AC7e9a71e2d5',
-  //   contractAbi: crossTokenABI,
-  // },
-  // {
-  //   chainId: 56,
-  //   chainName: 'Binance Smart Chain',
-  //   chainSymbol: 'BSC',
-  //   coin: 'BNB',
-  //   textColor: '#FFFFFF',
-  //   color: 'warning',
-  //   contractAddr: '0x50d6e5bea4CD1D9a166689f806CB744F0395041e',
-  //   contractAbi: crossTokenABI,
-  // },
-  // {
-  //   chainId: 513100,
-  //   chainName: 'EthereumFair',
-  //   chainSymbol: 'ETHF',
-  //   coin: 'ETHF',
-  //   textColor: '#FFFFFF',
-  //   color: 'primary',
-  //   contractAddr: '0xbF4564DeC02eF0A5EBCda3B398b7E1d8d0529236',
-  //   contractAbi: crossTokenABI,
-  // },
+  {
+    chainId: 7777777,
+    chainName: 'ZORA Network',
+    chainSymbol: 'Zora',
+    coin: 'ETH',
+    textColor: '#FFFFFF',
+    color: '#000000',
+    contractAddr: '0x111cdBF288C9613019c9c324D9E9AC7e9a71e2d5',
+    contractAbi: generalABI,
+  },
+  {
+    chainId: 56,
+    chainName: 'Binance Smart Chain',
+    chainSymbol: 'BSC',
+    coin: 'BNB',
+    textColor: '#FFFFFF',
+    color: 'warning',
+    contractAddr: '0x50d6e5bea4CD1D9a166689f806CB744F0395041e',
+    contractAbi: generalABI,
+  },
+  {
+    chainId: 513100,
+    chainName: 'DISChain',
+    chainSymbol: 'DIS',
+    coin: 'DIS',
+    textColor: '#FFFFFF',
+    color: 'primary',
+    contractAddr: '0x818B271ACa8885EE8246B023A8e324fA9c0C414D',
+    contractAbi: generalABI,
+  },
   {
     chainId: 168587773,
     chainName: 'BLAST Sepolia',
@@ -575,6 +576,6 @@ export const supportNetworks: NetModel[] = [
     textColor: '#FF00FF',
     color: 'primary',
     contractAddr: '0x50d6e5bea4CD1D9a166689f806CB744F0395041e',
-    contractAbi: crossTokenABI,
+    contractAbi: blastABI,
   },
 ]

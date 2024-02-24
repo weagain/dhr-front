84419eafb8e4cd7fab385e664d10ed5c742cb536
nnuauroral@163.com <herohero249>
2024-02-24 16:16:17 +0800

    format

--- vite.config.ts
+++ vite.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') 
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     host: '0.0.0.0',
